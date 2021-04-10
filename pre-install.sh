ad8396955d1970664c3edcad4d2d1f33a74e7443
AleksRex <a.bah80@yandex.ru>
2021-04-10 07:15:05 +0300

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -2,4 +2,8 @@
 
 pacstrap /mnt base base-devel linux linux-firmware vim nano git intel-ucode
 genfstab -U /mnt >> /mnt/etc/fstab
+cp -r Archlinux /mnt
 arch-chroot /mnt
+cd Archlinux
+chmod +x arch-install-uefi.sh
+./arch-install-uefi.sh
