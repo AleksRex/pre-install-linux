75d192d5f2c6aa90e3c250caee9754cab3546609
AleksRex <a.bah80@yandex.ru>
2021-04-10 18:53:04 +0300

    Update pre-install.sh

--- pre-install.sh
+++ pre-install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-pacstrap /mnt base base-devel linux linux-firmware vim nano git intel-ucode
+pacstrap /mnt base base-devel linux linux-firmware vim nano git intel-ucode 
 genfstab -U /mnt >> /mnt/etc/fstab
 cp -r Archlinux /mnt
 arch-chroot /mnt
 cd Archlinux
 chmod +x arch-install-uefi.sh
-./arch-install-uefi.sh
+#./arch-install-uefi.sh
