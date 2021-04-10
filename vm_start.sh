1c6e0d2c15fb35585af636fcc677c9c01d81d66c
AleksRex <a.bah80@yandex.ru>
2021-04-10 19:03:14 +0300

    Update vm_start.sh

--- vm_start.sh
+++ vm_start.sh
@@ -6,6 +6,6 @@ chmod -R o+x pre-install-linux
 ./pre-install-linux/vm_format.sh
 ./pre-install-linux/vm_btrfs_mount.sh
 ./pre-install-linux/pre-install.sh
-cp -r Archlinux/arch-install-uefi.sh /mnt
-chmod +x /mnt/arch-install-uefi.sh
-arch-chroot /mnt
+#cp -r Archlinux/arch-install-uefi.sh /mnt
+#chmod +x /mnt/arch-install-uefi.sh
+#arch-chroot /mnt
